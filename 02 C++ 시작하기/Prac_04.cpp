4fdeeb4aa8acaec011563275ba9296180e76d33e
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-09-13 08:52:27 +0900

    Update Prac_04.cpp

--- 02 C++ 시작하기/Prac_04.cpp
+++ 02 C++ 시작하기/Prac_04.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-int year_to_month(int);
-
 int main()
 {
     using namespace std;
@@ -10,12 +8,7 @@ int main()
     cout << "Enter your age: ";
     cin >> year;
 
-    cout << "당신의 나이는 월수로 " << year_to_month(year) << "입니다." << endl;
+    cout << "당신의 나이는 월수로 " << 12 * year << "입니다." << endl;
 
     return 0;
-}
-
-int year_to_month(int year)
-{
-    return year * 12;
 }
