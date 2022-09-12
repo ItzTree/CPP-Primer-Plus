d18b297f352959d30689bb66f10784119e076244
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-09-12 14:18:52 +0900

    Create Prac_03.cpp

---
+++ 02 C++ 시작하기/Prac_03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void print_three();
+void print_see();
+
+int main()
+{
+    using namespace std;
+
+    print_three();
+    print_three();
+    print_see();
+    print_see();
+
+    return 0;
+}
+
+void print_three()
+{
+    using namespace std;
+    cout << "Three blind mice" << endl;
+}
+
+void print_see()
+{
+    std::cout << "See how they run" << std::endl;
+}
