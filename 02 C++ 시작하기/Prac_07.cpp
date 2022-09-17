08f8c694e56ebee5089ca6890ac955c569b64789
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-09-17 10:22:10 +0900

    Create Prac_07.cpp

---
+++ 02 C++ 시작하기/Prac_07.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void show_time(int, int);
+
+int main()
+{
+    int h, m;
+    cout << "시간 값을 입력하시오: ";
+    cin >> h;
+    cout << "분 값을 입력하시오: ";
+    cin >> m;
+
+    show_time(h, m);
+
+    return 0;
+}
+
+void show_time(int h, int m)
+{
+    cout << "시각: " << h << ":" << m << endl;
+}
