a209d4b9678c82db7c2a9ff0ea0b6efc1cdac954
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-09-17 10:18:53 +0900

    Create Prac_06.cpp

---
+++ 02 C++ 시작하기/Prac_06.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int ly_to_AU(double);
+
+int main()
+{
+    using namespace std;
+
+    double ly;
+    cout << "광년 수: ";
+    cin >> ly;
+
+    cout << ly << " 광년은 " << ly_to_AU(ly) << " AU입니다." << endl;
+
+    return 0;
+}
+
+int ly_to_AU(double ly)
+{
+    return ly * 63240;
+}
