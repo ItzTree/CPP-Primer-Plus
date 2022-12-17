f0b6e53f7eb94cd94eb5bd411edc5e4a8acbcdb0
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-12-17 15:23:10 +0900

    Update 05_ourfunc.cpp

--- 02 C++ 시작하기/05_ourfunc.cpp
+++ 02 C++ 시작하기/05_ourfunc.cpp
@@ -1,33 +1,32 @@
+// ourfunc.cpp -- 사용자가 작성하는 함수를 정의한다
+
 #include <iostream>
 
-void simon(int);
+void simon(int);                // simon()을 위한 함수 원형
 
 int main()
 {
     using namespace std;
     
-    simon(3);
+    simon(3);                   // simon() 함수를 호출한다
     cout << "정수를 하나 고르시오: ";
     int count;
     cin >> count;
-    simon(count);
+    simon(count);               // simon() 함수를 다시 호출한다
     cout << "끝!" << endl;
 
     return 0;
 }
 
-void simon(int n)
+void simon(int n)               // simon() 함수를 정의한다
 {
     using namespace std;
     cout << "Simon 왈, 발가락을 " << n << "번 두드려라." << endl;
-}
+}                               // void형 함수에는 return 구문이 필요 없다
 
 /*
 Simon 왈, 발가락을 3번 두드려라.
 정수를 하나 고르시오: 10 
 Simon 왈, 발가락을 10번 두드려라.
 끝!
-
-<-- Content -->
-사용자 정의 함수
 */
