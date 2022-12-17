7244fcd9ca0ba833b5c89feae1ea67efc64af1bc
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-12-17 13:36:53 +0900

    Update 01_myfirst.cpp

--- 02 C++ 시작하기/01_myfirst.cpp
+++ 02 C++ 시작하기/01_myfirst.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
+// myfirst.cpp -- 메시지를 출력한다
 
-int main()
-{
-    using namespace std;
-    cout << "C++의 세계로 오십시오.";
-    cout << endl;
-    cout << "후회하지 않으실 겁니다!" << endl;
-    return 0;
-}
+#include <iostream>                             // 선행처리 지시자
+int main()                                      // 함수 머리
+{                                               // 함수 몸체의 시작
+    using namespace std;                        // 정의 가시화
+    cout << "C++의 세계로 오십시오.";             // 메시지 출력
+    cout << endl;                               // 새로운 행 시작
+    cout << "후회하지 않으실 겁니다!" << endl;    // 또 다른 메시지 출력
+    return 0;                                   // main()을 종료
+}                                               // 함수 몸체의 끝
 
 /*
 C++의 세계로 오십시오.
 후회하지 않으실 겁니다!
-
-<-- Content -->
-main()함수
-C++ 전처리기와 iostream 헤더파일
-이름 공간(namespace)
-cout
 */
